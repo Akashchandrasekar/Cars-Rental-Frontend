91b3d971b1b6032b7786d7e3746361122ba91d5d
AKASH CHANDRASEKAR <162828838+Akashchandrasekar@users.noreply.github.com>
2024-12-15 10:58:55 +0530

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cars-rental-project-backend.onrender.com/api",
+  baseURL: "https://carsrental-project-backend.onrender.com/api",
 });
 
 api.interceptors.request.use(
